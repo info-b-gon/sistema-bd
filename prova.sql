d8257e1519701b75c72d4eb7b00f17549e8abe3b
lucasgonv <lucgonmendes@gmail.com>
2026-09-10 11:46:31 -0300

    Add files via upload

--- prova.sql
+++ prova.sql
@@ -114,7 +114,7 @@ select c.nome, count(r) from cliente c join reserva r on c.id=r.id_cliente group
 -- Mostre o nome de cada cliente e a quantidade de reservas realizadas por ele. 
 
 select p.nome, count(c) from passeio p join reserva_passeio rp on p.id=rp.id_passeio join reserva r on rp.id_reserva=r.id join cliente c on r.id_cliente=c.id
-group by p.nome order by p.nome desc;
+group by p.nome order by c.count desc;
 
 -- Mostre o nome de cada passeio e a quantidade de clientes associados a ele. Utilize COUNT(), INNER JOIN, GROUP
 -- BY e ORDER BY, ordenando do maior para o menor. (1,5)
